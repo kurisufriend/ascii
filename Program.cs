207b28654f93f5c3931c0d076fa12f3c92c01a93
sidekek <admin@sidekek.ml>
2020-07-15 01:13:24 -0400

    is this how you do arguments?

--- Program.cs
+++ Program.cs
@@ -7,10 +7,23 @@ namespace kot
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("missing input argument, pass \'help\' for more info");
+                System.Environment.Exit(0);
+            }
+            if (args[0] == "help")
+            {
+                Console.WriteLine(@"maek ascii !!
+                       kot <input>
+input argument /must/ be of types: {jpg, jpeg, png, bmp,}");
+                System.Environment.Exit(0);
+            }
+            string path = args[0];
             string final = "";
             string[] scale = {".",",",":",";","+","*","?","%","S","#","@"};
             //Array.Reverse(scale);
-            var f = new Bitmap("./kurisu.jpg");
+            var f = new Bitmap(path);
             for (int i = 0; i < f.Height; i += 30)
             {
                 for (int z = 0; z < f.Width; z += 30)
